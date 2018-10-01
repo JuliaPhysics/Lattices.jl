851ddbf4e6ba4e906b3c80e3e8bf8464922115cb
Roger-luo <hiroger@qq.com>
2018-10-01 14:11:13 -0400

    config documenter

--- doc/make.jl
+++ doc/make.jl
@@ -1,1 +1,22 @@
-using Documenter
+using Documenter, Lattices
+
+makedocs(
+     modules = [Lattices],
+     format  = :html,
+     sitename = "Lattices",
+     pages = Any[
+         "Introduction"   => "index.md"
+     ],
+     # Use clean URLs, unless built as a "local" build
+     html_prettyurls = !("local" in ARGS),
+     # html_canonical = "https://juliadocs.github.io/Documenter.jl/latest/",
+)
+
+deploydocs(
+     repo = "git@github.com:Roger-luo/Lattices.jl.git",
+     target = "build",
+     julia = "1.0",
+     osname = "linux",
+     deps = nothing,
+     make = nothing,
+)
